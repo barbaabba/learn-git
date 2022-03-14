5b1fcf65ed6c5bba181cc2c9e47bdf35a2f6ae42
Baruch <Baruchbaz@gmail.com>
2022-03-14 16:25:28 +0200

    myFunc

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def myfunc():
+    print("my Function")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    myfunc()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
